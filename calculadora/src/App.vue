27410567f05ea32f7a873da322081ec6a8725414
Thacio Helmer <thacio.helmer@el.com.br>
2022-11-26 13:49:08 -0300

    Atualização curso #261122

---
+++ calculadora/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calulator";
+export default {
+  components: { Calculator },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "Roboto-Light";
+  src: url("./fonts/Roboto-Light.ttf");
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Roboto-Light", sans-serif;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #d3d3d3;
+}
+</style>
